2474572ddc68859b87a7fbd79ee3480a45b48195
Anton Davydov <antondavydov.o@gmail.com>
2017-09-01 01:22:08 +0300

    Display guides data for specific version

--- config.rb
+++ config.rb
@@ -166,6 +166,14 @@ helpers do
     end
   end
 
+  def guides_data(version = nil)
+    case version
+    when 1.0 then data.guides_1_0
+    else
+      data.guides
+    end
+  end
+
   def guides_navigation
     result = ''
 
@@ -213,7 +221,7 @@ helpers do
     version_text = version == 'head' ? nil : "/ #{link_to(version, "/guides/#{version}")} "
     page_title = metadata[:page]['title'].split(' - ').last
 
-    category = data.guides.categories.select do |c|
+    category = guides_data(version).categories.select do |c|
       c['pages'].map { |p| p['path'] }.include?(page.url.split('/').last)
     end.first
 
