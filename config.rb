7dfccd651f81f51a1a76a1107051ec807cebac36
Tim Riley <tim@riley.id.au>
2025-11-12 23:53:42 +1100

    Bump latest Hanami version

--- config.rb
+++ config.rb
@@ -105,11 +105,11 @@ helpers do
   end
 
   def hanami_version
-    "2.2.1"
+    "2.3.0"
   end
 
   def hanami_release_date
-    Date.parse("2024-11-13").strftime("%B %-d, %Y")
+    Date.parse("2025-11-12").strftime("%B %-d, %Y")
   end
 end
 
